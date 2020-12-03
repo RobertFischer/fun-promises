c35d7cc355b2c88780bd85fb76d1ef97f1f15eea
Robert Fischer <robert.fischer@k4connect.com>
2020-12-03 10:23:37 -0500

    docs(package documentation): added in package documentation to ./index.ts ./types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,15 @@
-/** @format */
+/**
+ * /* @format
+ *
+ * @format
+ */
+
+/**
+ * These are where we put all the type aliases, interfaces, and enumerations that are shared
+ * between multiple things, or may be useful to use without us.
+ *
+ * @packageDocumentation
+ */
 
 /**
  * Provides the type of elements within an `Iterable`, which may be wrapped in one or more promises.
