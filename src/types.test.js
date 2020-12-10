c0f92a4c32e2ca589aea9ffbd33b8c0cd5ab95f1
Robert Fischer <robert.fischer@k4connect.com>
2020-12-09 22:00:12 -0500

    feat(settlement): create types representing the result of a promise

---
+++ src/types.test.js
@@ -0,0 +1,38 @@
+/** @format */
+
+import { Fulfillment, Rejection } from "./types";
+import _ from "lodash";
+
+describe("Fulfillment", () => {
+	_.forEach([true, { foo: true }], (arg) => {
+		describe(`when passed the ${typeof arg} argument '${JSON.stringify(
+			arg
+		)}'`, () => {
+			const fulfillment = new Fulfillment(arg);
+
+			it("exposes the argument on the 'value' property", () => {
+				expect(fulfillment).toHaveProperty("value", arg);
+			});
+
+			it("has a 'status' property of 'fulfilled'", () => {
+				expect(fulfillment).toHaveProperty("status", "fulfilled");
+			});
+		});
+	});
+});
+
+describe("Rejection", () => {
+	_.forEach(["BOOM!", new Error("BOOM!")], (arg) => {
+		describe(`when passed the ${typeof arg} argument: '${arg}'`, () => {
+			const rejection = new Rejection(arg);
+
+			it("returns the argument in the 'reason' property", () => {
+				expect(rejection).toHaveProperty("reason", arg);
+			});
+
+			it("has a 'status' property whose value is 'rejected'", () => {
+				expect(rejection).toHaveProperty("status", "rejected");
+			});
+		});
+	});
+});
