f129cda2da3a1daffcc2e9c81deebcaf51e063d6
Robert Fischer <robert.fischer@k4connect.com>
2020-12-11 13:23:29 -0500

    build(release): get rid of the NPM package asset and fix RN

--- release.config.js
+++ release.config.js
@@ -5,7 +5,6 @@ const path = require("path");
 const { name } = require("./package.json");
 
 const distributions = {
-	NPM: `dist/${name}-v*.tgz`,
 	"Browser - Single File": "./dist/browser.index.js.xz",
 	"Browser - with Source Maps": "./dist/browser.tar.xz",
 	"ES6 AMD - Single File": "./dist/amd.index.js.xz",
@@ -16,7 +15,7 @@ const distributions = {
 	"Node 10": "./dist/v10.tar.xz",
 	"Node 12": "./dist/v12.tar.xz",
 	"Node 14": "./dist/v14.tar.xz",
-	"React Native": "./dist/react-native-tar.xz",
+	"React Native": "./dist/react-native.tar.xz",
 };
 
 const assets = _.concat(
